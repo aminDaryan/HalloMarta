9032f9d69bad8d6cf6228c4af960d9061f0a6eab
Amin Daryan <amindarian@gmail.com>
2021-06-14 02:48:04 +0430

    test-ds added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -21,7 +21,7 @@ export default function Main() {
     setData(null);
     const form = new FormData(e.target);
     const input = form.get('input');
-    console.log('input: ', input);
+
     const response = await axios.get(
       `https://jsonmock.hackerrank.com/api/stocks?date=${input}`
     );
@@ -35,11 +35,16 @@ export default function Main() {
         <Input
           type="text"
           name="input"
+          data-testid="app-input"
           placeholder="5-January-2000"
           pattern="^(([0-9])|([0-2][0-9])|([3][0-1]))-(january|february|march|april|may|june|july|august|september|october|november|december)-\d{4}$"
           required
         />
-        <SearchButton type="submit" value="Search" />
+        <SearchButton
+          type="submit"
+          data-testid="submit-button"
+          value="Search"
+        />
       </InputContainer>
       <ResultsContainer>
         {data &&
